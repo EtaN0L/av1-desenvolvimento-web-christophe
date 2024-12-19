eeff8a9d6c675790ecc9753ca230ea628afaa8f1
EtaN0L <128407620+EtaN0L@users.noreply.github.com>
2024-12-19 16:30:20 -0300

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/grand-theft-auto-v-seeklogo.svg 	modified:   src/app/app.config.ts 	modified:   src/app/sidebar/sidebar.component.html 	modified:   src/app/sidebar/sidebar.component.ts 	modified:   src/app/top/top.component.html 	new file:   src/custom-theme.scss 	modified:   src/index.html 	modified:   src/styles.css

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
