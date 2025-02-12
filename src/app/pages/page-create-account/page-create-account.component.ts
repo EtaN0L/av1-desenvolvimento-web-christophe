9ba27cb5604e1a2964280a92b3936d56ff1447da
EtaN0L <128407620+EtaN0L@users.noreply.github.com>
2025-02-12 02:52:36 -0300

    modified:   src/app/app.config.ts 	modified:   src/app/pages/page-create-account/page-create-account.component.css 	modified:   src/app/pages/page-create-account/page-create-account.component.html 	modified:   src/app/pages/page-create-account/page-create-account.component.ts 	modified:   src/app/progress-bar/progress-bar.component.scss

--- src/app/pages/page-create-account/page-create-account.component.ts
+++ src/app/pages/page-create-account/page-create-account.component.ts
@@ -1,11 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-page-create-account',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './page-create-account.component.html',
   styleUrl: './page-create-account.component.css'
 })
 export class PageCreateAccountComponent {
+  data = {username: '', email: '', password: '', re_password: ''};
 
+  constructor(private http: HttpClient){}
+
+  createAccount(){
+    this.http
+      .post('http://localhost:8000/auth/users/', this.data)
+      .subscribe((answer) => {
+        console.log(answer);
+      });
+  }
 }
